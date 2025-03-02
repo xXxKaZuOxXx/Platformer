51ea379f56b14fdf530c017f29acb3617a74f92c
Nikita <sotskov02@gmail.com>
2025-03-02 22:19:22 +0300

    DZ4

--- New Unity Project/Assets/Scripts/PixelCrew/Hero.cs
+++ New Unity Project/Assets/Scripts/PixelCrew/Hero.cs
@@ -5,36 +5,47 @@ using UnityEngine;
 public class Hero : MonoBehaviour
 {
     private Vector2 _direction;
-
+    private Rigidbody2D _rigidbody;
     [SerializeField]private float _speed;
+    [SerializeField] private float _jumpspeed;
+  
+
+    [SerializeField] private LayerCheck _groundCheck;
+
+    private void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+    }
     public void SetDirection(Vector2 direction)
     {
         _direction = direction;
     }
-    private void Update()
+    private void FixedUpdate()
     {
-        //if(_direction.x != 0 || _direction.y != 0)
-        //{
-        //    var deltaX = _direction.x * _speed * Time.deltaTime;
-        //    var newXPosition = transform.position.x + deltaX;
-        //    var deltaY = _direction.y * _speed * Time.deltaTime;
-        //    var newYPosition = transform.position.y + deltaY;
-        //    transform.position = new Vector3(newXPosition, newYPosition, transform.position.z);
-        //}
-        if (_direction.x != 0)
+        _rigidbody.velocity = new Vector2(_direction.x * _speed, _rigidbody.velocity.y);
+        var IsJumping = _direction.y > 0;
+        if(IsJumping )
         {
-            var delta = _direction * _speed * Time.deltaTime;
-            var newXPosition = transform.position.x + delta.x;
-            transform.position = new Vector3(newXPosition, transform.position.y, transform.position.z);
+            if( IsGrounded())
+            {
+                _rigidbody.AddForce(Vector2.up * _jumpspeed, ForceMode2D.Impulse);
+            }
         }
-        if (_direction.y != 0)
+        else if (_rigidbody.velocity.y > 0)
         {
-            var delta = _direction * _speed * Time.deltaTime;
-            var newYPosition = transform.position.y + delta.y;
-            transform.position = new Vector3(transform.position.x, newYPosition, transform.position.z);
+            _rigidbody.velocity = new Vector2(_rigidbody.velocity.x, _rigidbody.velocity.y * 0.5f);
         }
-
     }
+    private bool IsGrounded()
+    {
+        return _groundCheck.IsTouchingLayer;
+        
+    }
+    //private void OnDrawGizmos()
+    //{
+    //    Gizmos.color = IsGrounded() ? Color.green : Color.red;
+    //    Gizmos.DrawSphere(transform.position, 0.3f);
+    //}
     public void SaySomething()
     {
         Debug.Log("Some");
