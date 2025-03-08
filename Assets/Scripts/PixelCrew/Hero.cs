b3c443baa167b5ba755a06234da81fb414c56b85
Nikita <sotskov02@gmail.com>
2025-03-08 04:17:23 +0300

    DZ6

--- Assets/Scripts/PixelCrew/Hero.cs
+++ Assets/Scripts/PixelCrew/Hero.cs
@@ -6,17 +6,26 @@ public class Hero : MonoBehaviour
 {
     private Vector2 _direction;
     private Rigidbody2D _rigidbody;
+    private Animator _animator;
+    private SpriteRenderer _sprite;
+
     [SerializeField]private float _speed;
     [SerializeField] private float _jumpspeed;
-
+   
     public int Score { get; set; } = 0;
-  
+
+    private static readonly int IsGraund = Animator.StringToHash("Isground");
+    private static readonly int Verticalvelocity = Animator.StringToHash("Verticalvelocity");
+    private static readonly int Isrunning = Animator.StringToHash("Isrunning");
+
 
     [SerializeField] private LayerCheck _groundCheck;
 
     private void Awake()
     {
         _rigidbody = GetComponent<Rigidbody2D>();
+        _animator = GetComponent<Animator>();
+        _sprite = GetComponent<SpriteRenderer>();
     }
     public void SetDirection(Vector2 direction)
     {
@@ -26,6 +35,7 @@ public class Hero : MonoBehaviour
     {
         _rigidbody.velocity = new Vector2(_direction.x * _speed, _rigidbody.velocity.y);
         var IsJumping = _direction.y > 0;
+        var isGraunded = IsGrounded();
         if(IsJumping )
         {
             if( IsGrounded())
@@ -36,6 +46,21 @@ public class Hero : MonoBehaviour
         else if (_rigidbody.velocity.y > 0)
         {
             _rigidbody.velocity = new Vector2(_rigidbody.velocity.x, _rigidbody.velocity.y * 0.5f);
+        }
+        _animator.SetBool(IsGraund, isGraunded);
+        _animator.SetFloat(Verticalvelocity, _rigidbody.velocity.y);
+        _animator.SetBool(Isrunning, _direction.x != 0);
+        UpdateSpriteDirection();
+    }
+    private void UpdateSpriteDirection()
+    {
+        if (_direction.x > 0)
+        {
+            _sprite.flipX = false;
+        }
+        else if (_direction.x < 0)
+        {
+            _sprite.flipX = true;
         }
     }
     private bool IsGrounded()
---
+++ Assets/Scripts/PixelCrew/SpriteAnimation.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace PixelCrew
+{
+    [RequireComponent(typeof(SpriteRenderer))]
+    public class SpriteAnimation : MonoBehaviour
+    {
+        [SerializeField] private int _frameRate;
+        [SerializeField] private bool _loop;
+        [SerializeField] private Sprite[] _sprites;
+        [SerializeField] private UnityEvent _onComplete;
+
+        private SpriteRenderer _renderer;
+        private float _secondsPerFrame;
+        private int _currentSprite;
+        private float _nextFrameTime;
+        private bool _isPlaying = true;
+        private void Start()
+        {
+            _secondsPerFrame = 1f / _frameRate;
+            _renderer = GetComponent<SpriteRenderer>();
+            _nextFrameTime = Time.time + _secondsPerFrame;
+        }
+
+        private void Update()
+        {
+            if(_nextFrameTime > Time.time || !_isPlaying)
+            {
+                return;
+            }
+            if(_currentSprite >= _sprites.Length)
+            {
+                if(_loop)
+                {
+                    _currentSprite = 0;
+                }
+                else
+                {
+                    _isPlaying = false;
+                    _onComplete?.Invoke();
+                    return;
+                }
+            }
+            
+                _renderer.sprite = _sprites[_currentSprite];
+                _nextFrameTime += _secondsPerFrame;
+                _currentSprite++;
+            
+        }
+
+    }
+}
+
