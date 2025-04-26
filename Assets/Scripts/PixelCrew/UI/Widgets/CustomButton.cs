7b060b83d42714716c36b978a8db5fce58deccfd
Nikita <sotskov02@gmail.com>
2025-04-27 02:20:50 +0300

    DZ 16

---
+++ Assets/Scripts/PixelCrew/UI/Widgets/CustomButton.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CustomButton : Button
+{
+    [SerializeField] private GameObject _normal;
+    [SerializeField] private GameObject _select;
+
+    protected override void DoStateTransition(SelectionState state, bool instant)
+    {
+        base.DoStateTransition(state, instant);
+        _normal.SetActive(state != SelectionState.Pressed);
+        _select.SetActive(state == SelectionState.Pressed);
+         
+    }
+}
---
+++ Assets/Scripts/PixelCrew/UI/Widgets/Editor/CustomButtonEditor.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEditor.UI;
+using UnityEngine;
+
+[CustomEditor(typeof(CustomButton), true)]
+[CanEditMultipleObjects]
+public class CustomButtonEditor : ButtonEditor
+{
+    public override void OnInspectorGUI()
+    {
+        EditorGUILayout.PropertyField(serializedObject.FindProperty("_normal"));
+        EditorGUILayout.PropertyField(serializedObject.FindProperty("_select"));
+        serializedObject.ApplyModifiedProperties();
+        base.OnInspectorGUI();
+    }
+}
